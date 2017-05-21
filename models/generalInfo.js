ca6fd226a2f2ac74d729a67e57b58a50ed06505d
ako80218 <andrew@andrewotto.com>
2017-05-21 08:55:57 -0400

    gallery in dev

--- models/generalInfo.js
+++ models/generalInfo.js
@@ -1,6 +1,19 @@
 module.exports = {
 	siteTitle:"LDO Creates",
 	ldoContactEmail:"lynnotto@outlook.com",
-	cardCategories:["holiday", "birthday", "general", "thank-you"]
+	cardCategories:["holiday", "birthday", "general", "thank-you"],
+	cardImages:{
+		thankYou:{
+			thumbnails:{
+				front:"",
+				gallery01:"",
+				gallery02:""
+
+			},
+			fullSize:{
+
+			},
+		}
+	}
 
 }
