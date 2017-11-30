e13fb816e20342ac1243a8eed49491c073a8bb8e
ako80218 <andrew@andrewotto.com>
2017-11-29 14:34:52 -1000

    added hero image

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require('gulp');
 //Include plugins
 var imagemin = require('gulp-imagemin');
 var imageminMozjpeg = require('imagemin-mozjpeg');
-
+var sass = require('gulp-sass');
 
 //Optimize our JPEGS
 gulp.task('mozjpeg', () =>
@@ -15,8 +15,19 @@ gulp.task('mozjpeg', () =>
     .pipe(gulp.dest('public/images'))
 );
 
+//Render .sass to .css
+gulp.task('sass', function(){
+	gulp.src('stylesheets/*.sass')
+	.pipe(sass())
+	.pipe(gulp.dest('public/stylesheets'))
+}); 
+
+gulp.task('watch', function(){
+	gulp.watch("public/stylesheets/*.sass", ['sass']);
+});
+
 
 
 
 //Driver is 'default' which runs our tasks.
-gulp.task('default', ['mozjpeg']);
+gulp.task('default', ['mozjpeg', 'sass', 'watch']);
