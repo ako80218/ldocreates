6e18018ef2099248448af5b16a91bb7a74c3f910
ako80218 <andrew@andrewotto.com>
2018-05-08 10:49:31 -1000

    gallery js changed

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
 var imageminMozjpeg = require('imagemin-mozjpeg');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
 
 //Optimize our JPEGS
 gulp.task('mozjpeg', () =>
@@ -27,8 +28,18 @@ gulp.task('watch', function(){
 	gulp.watch("public/stylesheets/*.sass", ['sass']);
 });
 
+gulp.task('serve', function(){
+    browserSync.init({
+        proxy: "localhost:3000",
+        files: ["public/**/*.*", "views/*.jade", "routes/*.js", "models/generalInfo.js", "functions/*.js"],
+    });
+});
+
+
+
+
 
 
 
 //Driver is 'default' which runs our tasks.
-gulp.task('default', ['mozjpeg', 'sass', 'watch']);
+gulp.task('default', ['sass', 'watch', 'serve']);
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -57,17 +57,16 @@ $(function(){
 
 	})
 
-	$("#slideshow > div:gt(0)").hide();
+	// $("#slideshow > div:gt(0)").hide();
 
-	setInterval(function() { 
-	  $('#slideshow > div:first')
-	    .fadeOut(1000)
-	    .next()
-	    .fadeIn(1000)
-	    .end()
-	    .appendTo('#slideshow');
-	},  3000);
-
+	// setInterval(function() { 
+	//   $('#slideshow > div:first')
+	//     .fadeOut(1000)
+	//     .next()
+	//     .fadeIn(1000)
+	//     .end()
+	//     .appendTo('#slideshow');
+	// },  3000);
 
 
 
